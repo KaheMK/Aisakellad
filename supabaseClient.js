9d5595c4206126e319f7c5342bced752e2358c6d
KaheM <kaarelkaarle@gmail.com>
2025-12-18 17:09:59 +0200

    Add files via upload

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+// supabaseClient.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+export const supabase = createClient("https://mkghndaoiqfapflqcuwu.supabase.co", 
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZ2huZGFvaXFmYXBmbHFjdXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MjE5MzgsImV4cCI6MjA4MTI5NzkzOH0.FZ7bq2349JlrGkj1KwrTGms9Ayii26OmOFtUocP_-1M"
+);
